2c1ad95f34a9f08cd3ef00640fd5684c1fa7fded
carlinf <carlinfernandes077@gmail.com>
2022-07-26 23:57:49 +0530

    added Styling and Components

--- react-photo-search/src/App.js
+++ react-photo-search/src/App.js
@@ -1,10 +1,13 @@
-
-import './App.css';
+import "./App.css";
+import SearchPics from "./searchPics";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <h1 className="title">React Photo Search</h1>
+        <SearchPics />
+      </div>
     </div>
   );
 }
