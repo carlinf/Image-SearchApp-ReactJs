71a1f258614359b58f2a18b0a9a3056a3a03ddfa
carlinf <carlinfernandes077@gmail.com>
2022-07-27 19:07:40 +0530

    Final commit running with no errors

--- react-photo-search/src/App.js
+++ react-photo-search/src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import SearchPics from "./searchPics";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="title">React Photo Search</h1>
+        <h1 className="title">Image Search</h1>
         <SearchPics />
       </div>
     </div>
   );
 }
+
+
 
 export default App;
